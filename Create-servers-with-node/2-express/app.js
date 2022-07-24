9ac5508e866844299376c1ab2f175f6870d23b17
nok-chan <HrithikSarkar45@protonmail.com>
2022-07-24 20:02:31 +0530

    navbar node

---
+++ Create-servers-with-node/2-express/app.js
@@ -0,0 +1,28 @@
+const http = require("http");
+
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+
+    const url = req.url;
+    if(url === "/") {
+        res.writeHead(200, {"content-type": "text/html"});
+        res.write(`homePage`);
+        res.end();
+    }
+    else if(url === "/about") {
+        res.writeHead(200, {"content-type": "text/html"});
+        res.write(`<h1>This is the about page</h1>`);
+        res.end();
+    }
+    else {
+        res.writeHead(404, {"content-type": "text/html"});
+        res.write(`<h1>Page not Found!</h1>`);
+        res.end();
+    }
+
+});
+
+server.listen(5005, () => {
+    console.log(`Server is listening at port 5005`);
+});
