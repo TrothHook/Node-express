184df3315ec9858778ce77ad182d9ecfdb99f78a
nok-chan <HrithikSarkar45@protonmail.com>
2022-09-07 23:59:44 +0530

    data filtering in the controller only: pagination, numeric filtering

--- 04-Store-API/1-overview.js
+++ 04-Store-API/1-overview.js
@@ -10,3 +10,23 @@
 
 // In node there is a method known as exit. In our case it is useful -> why? Because if we are successful in populating the database with our data, we might as well terminate the whole process -> we dont need the file to be running. And if there is an error, we will exit by displaying an error code.
 
+
+// The setup is going to be more complex, than just hardcoding the name or any of the other property values.
+
+// We won't be sending responses based on hardcoded filter values
+
+// Let's start with our request, more specifically query params -> that is how we will get our data
+
+// http://hn.algolia.com/api/v1/search?query=... or http://hn.algolia.com/api/v1/search_by_date?query=... 
+
+// First we need to understand how can we send those query string parameters from POSTMAN
+
+// Regex, query operator -> mongodb docs
+
+// Next we will configure the option and limit, with this it will provide users an option to choose a page.
+
+// skip works the same way as limits, except that it skips the first items in the response.
+
+// use skip and limit for pagination functionality.
+
+// First thing we will do for numericFilters is operator map
--- 04-Store-API/controllers/products.js
+++ 04-Store-API/controllers/products.js
@@ -1,17 +1,96 @@
 const Product = require("../models/products");
 
 const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({ price: { $lt: 50 } })
+    .sort("price")
+    .select("name price")
+    .limit(10)
+    .skip(1);
 
-    throw new Error(`testing async errors`);
-
-    res.status(200).json({msg:`products testing route`});
-}
+  res.status(200).json({ products, nbHits: products.length });
+};
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg:`products route`});
-}
+  const { featured, name, company, sort, fields, numericFilters } = req.query;
+
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === `true` ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+
+    const regEx = /\b(<|>|=|>=|<=)\b/g;
+
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    const options = [`price`, `rating`];
+
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if(options.includes(field)) {
+        queryObject[field] = {[operator]:Number(value)}
+      }
+    });
+  }
+
+  console.log(queryObject);
+
+  // const products = await Product.find(queryObject);
+
+  // res.status(200).json({ products, nbHits: products.length });
+
+  let results = Product.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    results = results.sort(sortList);
+  }
+
+  // if the user didn't pass in the sort key, so default sort based on the times, when those items were created.
+  else {
+    results = results.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    results = results.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  results = results.skip(skip).limit(limit);
+
+  // 23 total items in the database
+  // if the limit is 4, then  7+7+7+2 = 23 ==> 4 pages intotal
+
+  const products = await results;
+
+  res.status(200).json({ products, nbHits: products.length });
+};
 
 module.exports = {
-    getAllProductsStatic, 
-    getAllProducts
+  getAllProductsStatic,
+  getAllProducts,
 };
