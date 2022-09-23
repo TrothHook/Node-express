3500fc480b9a6b0d718f2f4e9758d8f3722f8fef
nok-chan <HrithikSarkar45@protonmail.com>
2022-09-23 20:35:59 +0530

    jobs-API

--- 06-Jobs-API/controllers/jobs.js
+++ 06-Jobs-API/controllers/jobs.js
@@ -1,4 +1,6 @@
-
+const Job = require("../models/jobs");
+const {StatusCodes} = require("http-status-codes");
+const {BadRequestError, NotFoundError} = require("../errors/index");
 
 const getAllJobs = async (req, res) => {
     res.send(`get all jobs`);
@@ -7,6 +9,9 @@ const getJob = async (req, res) => {
     res.send(`get job`);
 }
 const createJob = async (req, res) => {
+    // At the moment, we are missing the user -> where is the user located? It is located in the req.user -> And what are we really looking for is the that user's id
+    req.body.createdBy = req.user.userId;
+    const job = await Job.create(req.body);
     res.json(req.body);
 }
 const updateJob = async (req, res) => {
